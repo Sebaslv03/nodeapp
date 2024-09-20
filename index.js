fcf987115b827b3037a7f857d0b4605864c97bca
Sebaslv03 <99445928+Sebaslv03@users.noreply.github.com>
2024-09-20 13:10:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! adrian maricon')
+  res.send('Hello World! esto es prueba')
 })
 
 app.listen(port, () => {
